5370fad01c678e797358983901fc7346c467fbb7
thomasgalliker <thomas@bluewin.ch>
2025-03-16 17:21:59 +0100

    Use GetConstructors only for NETSTANDARD1_0

--- DependencyResolver/Extensions/TypeExtensions.cs
+++ DependencyResolver/Extensions/TypeExtensions.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
-using System.Text;
 
 namespace DependencyResolver
 {
     internal static class TypeExtensions
     {
+#if NETSTANDARD1_0
         internal static IEnumerable<ConstructorInfo> GetConstructors(this Type type)
         {
             return type.GetTypeInfo().DeclaredConstructors;
         }
+#endif
     }
 }
