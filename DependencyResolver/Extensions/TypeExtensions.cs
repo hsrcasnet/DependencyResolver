d5c6cd28d2d3bf7ef5e31af22a4d69fb2991b62b
thomasgalliker <thomas@bluewin.ch>
2025-03-16 17:20:42 +0100

    Replace tfm "net45" with "netstandard1.0"

---
+++ DependencyResolver/Extensions/TypeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace DependencyResolver
+{
+    internal static class TypeExtensions
+    {
+        internal static IEnumerable<ConstructorInfo> GetConstructors(this Type type)
+        {
+            return type.GetTypeInfo().DeclaredConstructors;
+        }
+    }
+}
